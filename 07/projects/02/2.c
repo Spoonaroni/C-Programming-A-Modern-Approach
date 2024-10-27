83f7dbeeafcf636c36e5bf5aba07dc2b7814e158
Spoonaroni <163045653+Spoonaroni@users.noreply.github.com>
2024-10-27 17:57:57 -0400

    Finish project 7.02

--- 07/projects/02/2.c
+++ 07/projects/02/2.c
@@ -1,8 +1,26 @@
+/* Prints a table of squares using a for statement */
+
 #include <stdio.h>
 
 int main(void)
 {
+	int i, n;
 
+	printf("This program prints a table of squares. \n") ;
+	printf("Enter number of entries in table: ");
+	scanf("%d", &n);
+	getchar(); // to keep buffer from holding initial \n
+
+	for (i = 1; i <= n; i++)
+	{
+		printf("%10d%10d\n", i, i * i);
+		if (i % 24 == 0)
+		{
+			printf("Press Enter to continue...");
+			while (getchar() != '\n'); // prevents skipping if more than one char
+				;
+		}
+	}
 
 	return 0;
 }
--- 07/projects/02/README.md
+++ 07/projects/02/README.md
@@ -1,5 +1,13 @@
 ## Project 7.02
 
+Modify the `square2.c` program of Section 6.3 so that is pauses after every 24 squares and displays the following message:
 
+```
+Press Enter to continue...
+```
+
+After displaying the message, the program should use `getchar` to read a character. `getchar` won't allow the program to continue until the user presses the Enter key.
 
 ## Solution
+
+See `2.c`
