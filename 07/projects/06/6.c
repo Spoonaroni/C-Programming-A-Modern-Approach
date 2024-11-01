72c4ec5231bf04da4e8f90704deb3cac22e71406
Spoonaroni <163045653+Spoonaroni@users.noreply.github.com>
2024-11-01 17:20:06 -0400

    Finish project 7.06

--- 07/projects/06/6.c
+++ 07/projects/06/6.c
@@ -2,7 +2,12 @@
 
 int main(void)
 {
-
+	printf("%d\n", (int) sizeof(int));
+	printf("%hd\n", (short) sizeof(short));
+	printf("%ld\n", (long) sizeof(long));
+	printf("%f\n",  (float) sizeof(float));
+	printf("%lf\n", (double) sizeof(double));
+	printf("%Lf\n", (long double) sizeof(long double));
 
 	return 0;
 }
--- 07/projects/06/README.md
+++ 07/projects/06/README.md
@@ -1,5 +1,7 @@
 ## Project 7.06
 
-
+Write a program that prints the values of `sizeof(int)`, `sizeof(short)`, `sizeof(long)`, `sizeof(float)`, `sizeof(double)`, `sizeof(long double)`.
 
 ## Solution
+
+See `6.c`
