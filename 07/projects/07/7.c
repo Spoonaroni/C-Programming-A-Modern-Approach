e3b6cdc6e54f6db72edd9c415d8158c36dbc8df4
Spoonaroni <163045653+Spoonaroni@users.noreply.github.com>
2024-11-01 19:15:54 -0400

    Finish project 7.07

--- 07/projects/07/7.c
+++ 07/projects/07/7.c
@@ -2,7 +2,33 @@
 
 int main(void)
 {
+    int num1, denom1, num2, denom2, result_num, result_denom;
+	char sign;
 
+    printf("Enter two fractions separated by +, -, *, or /: "); 
+    scanf("%d/%d %c %d/%d", &num1, &denom1, &sign, &num2, &denom2);
 
-	return 0;
+	switch (sign)
+	{
+		case '+':
+			result_num = num1 * denom2 + num2 * denom1;
+		    result_denom = denom1 * denom2;
+			break;
+		case '-':
+			result_num = num1 * denom2 - num2 * denom1;		
+		    result_denom = denom1 * denom2;
+			break;
+		case '*':
+			result_num = num1 * num2;
+			result_denom = denom1 * denom2;
+			break;
+		case '/':
+			result_num = num1 * denom2;
+			result_denom = num2 * denom1;
+			break;
+	}
+
+    printf("Result: %d/%d\n", result_num, result_denom); 
+
+    return 0;
 }
