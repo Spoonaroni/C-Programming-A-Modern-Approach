0889a6a4f06e9b5dd5c0ce30375d370bf9c8c0f2
Spoonaroni <163045653+Spoonaroni@users.noreply.github.com>
2024-11-02 15:28:42 -0400

    Finish project 7.10

--- 07/projects/10/10.c
+++ 07/projects/10/10.c
@@ -1,8 +1,20 @@
+#include <ctype.h>
 #include <stdio.h>
 
 int main(void)
 {
+	int vCount = 0;
+	char c;
 
+	printf("Enter a sentence: ");
+	while ((c = getchar()) != '\n')
+	{
+		c = toupper(c);
+		if (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
+			vCount++;
+	}
+
+	printf("Your sentence contains %d vowels.\n", vCount);
 
 	return 0;
 }
