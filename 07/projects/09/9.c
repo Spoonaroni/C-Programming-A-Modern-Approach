b6454fa856c295aa7d0ea5b1310d4e903371652a
Spoonaroni <163045653+Spoonaroni@users.noreply.github.com>
2024-11-02 14:37:30 -0400

    Finish project 7.09

--- 07/projects/09/9.c
+++ 07/projects/09/9.c
@@ -1,8 +1,20 @@
+#include <ctype.h>
 #include <stdio.h>
 
 int main(void)
 {
+	int hour, min;
+	char c;
 
+	printf("Enter a 12-hour time: ");
+	scanf("%d : %d %c", &hour, &min, &c);
+
+	if (hour == 12)
+		hour = 0;
+	if (toupper(c) == 'P')
+		hour += 12;
+
+	printf("Equivalent 24-hour time: %.2d:%.2d\n", hour, min);
 
 	return 0;
 }
--- 07/projects/09/README.md
+++ 07/projects/09/README.md
@@ -1,5 +1,14 @@
 ## Project 7.09
 
+Write a program that asks that user for a 12-hour time, then displays the time in 24-hour form:
 
+```
+Enter a 12-hour time: 9:11 PM
+Equivalnet 24-hour time: 21:11
+```
+
+See Programming Porject 8 for a description of the input fomat.
 
 ## Solution
+
+See `9.c`
