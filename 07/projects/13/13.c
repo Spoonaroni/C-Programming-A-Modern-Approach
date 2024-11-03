a6305bef3045737e82b3912115cfff424c8f2d43
Spoonaroni <163045653+Spoonaroni@users.noreply.github.com>
2024-11-03 14:23:58 -0500

    Finish project 7.13

--- 07/projects/13/13.c
+++ 07/projects/13/13.c
@@ -2,7 +2,22 @@
 
 int main(void)
 {
+	float avg;
+	int len = 0, count = 1; //HACK: Cheaty way of keeping track of the last word
+	char c;
 
+	printf("Enter a sentence: ");
+
+	while ((c = getchar()) != '\n')
+	{
+		if (c == ' ')
+			count++;
+		else
+			len++;
+	}
+
+	avg = (float) len / (float) count;
+	printf("Average word length: %.1f\n", avg);
 
 	return 0;
 }
