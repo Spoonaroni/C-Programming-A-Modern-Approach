27048b433463ad35bf006ef4864dac51b3429b9c
Spoonaroni <163045653+Spoonaroni@users.noreply.github.com>
2024-11-03 18:16:19 -0500

    Finish project 7.15

--- 07/projects/15/15.c
+++ 07/projects/15/15.c
@@ -2,7 +2,18 @@
 
 int main(void)
 {
+	float d = 1;
+	int n;
 
+	printf("Enter a positive integer: ");
+	scanf("%d", &n);
+
+	for (int i = n; i > 0; i--)
+	{
+		d *= i;
+	}
+
+	printf("Factorial of %d: %f\n", n, d);
 
 	return 0;
 }
