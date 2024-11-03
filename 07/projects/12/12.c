a276ab24bc45ca3a811b9e4cb17427c1f1251b64
Spoonaroni <163045653+Spoonaroni@users.noreply.github.com>
2024-11-03 13:54:03 -0500

    Finish project 7.12

--- 07/projects/12/12.c
+++ 07/projects/12/12.c
@@ -2,7 +2,38 @@
 
 int main(void)
 {
+	float n, otherN;
+	char c;
 
+	printf("Enter an expression: ");
+	scanf("%f", &n);
+
+	while ((c = getchar()) != '\n')
+	{
+		switch (c)
+		{
+			case '+':
+				scanf("%f", &otherN);
+				n += otherN;
+				break;
+			case '-':
+				scanf("%f", &otherN);
+				n -= otherN;
+				break;
+			case '*':
+				scanf("%f", &otherN);
+				n *= otherN;
+				break;
+			case '/':
+				scanf("%f", &otherN);
+				n /= otherN;
+				break;
+			default:
+				break;
+		}
+	}
+
+	printf("Value of expression: %f \n", n);
 
 	return 0;
 }
