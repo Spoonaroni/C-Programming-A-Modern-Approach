37443968c285933292dedea379458a6c124e64c2
Spoonaroni <163045653+Spoonaroni@users.noreply.github.com>
2024-11-03 16:25:02 -0500

    Finish project 7.14

--- 07/projects/14/14.c
+++ 07/projects/14/14.c
@@ -1,8 +1,23 @@
+#include <math.h>
 #include <stdio.h>
 
 int main(void)
 {
+	int x;
+	double y = 1.0, xy, avg = 1.0;
 
+	printf("Enter a positive number: ");
+	scanf("%d", &x);
+
+	do
+	{
+		y = avg;
+		xy = x / y;
+		avg = (y + xy) / 2;
+	}
+	while (fabs(avg - y) > (0.00001 * avg));
+		
+	printf("Square root: %.5lf\n", avg);
 
 	return 0;
 }
