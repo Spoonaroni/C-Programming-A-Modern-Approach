d724b30ec2b866eb7dd8342fb042b14deb207313
Spoonaroni <163045653+Spoonaroni@users.noreply.github.com>
2024-11-10 18:16:45 -0500

    Finish project 8.01
    This took way too long. Remeber to initialize your variables folks!

--- 08/projects/01/1.c
+++ 08/projects/01/1.c
@@ -1,8 +1,42 @@
+/* Checks numbers for repeated digits */
+
+#include <stdbool.h>      /* C99 only */
 #include <stdio.h>
 
-int main(void)
-{
+int main(void) {
 
+    bool digit_seen[10] = {false}, 
+         repeated[10] = {false};
+    int digit, d = 0, n;
 
-	return 0;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    while (n > 0)
+	{
+        digit = n % 10;
+        if (digit_seen[digit])
+		{
+            repeated[digit] = true;
+			d++;
+		}
+        digit_seen[digit] = true;
+        n /= 10;
+    }
+
+	if (d > 0)
+	{
+		printf("Repeated digit(s): ");
+		for (int i = 0; i < 10; i++)
+			{
+		        if (repeated[i])
+		            printf("%d ", i);
+		    }
+	}
+	else
+		printf("No digits repeated");
+
+    printf("\n");
+
+    return 0;
 }
