ad6bf966217a3b52a5487a416ffa865fd097cdbc
Spoonaroni <163045653+Spoonaroni@users.noreply.github.com>
2024-11-16 18:01:54 -0500

    Finish project 8.06

--- 08/projects/06/6.c
+++ 08/projects/06/6.c
@@ -1,8 +1,50 @@
+#include <ctype.h>
 #include <stdio.h>
+
+#define N ((int) (sizeof(message) / sizeof(message[0])))
 
 int main(void)
 {
+	char message[50] = {0}, c;
+	int i = 0;
 
+	printf("Enter message: ");
+	while ((c = getchar()) != '\n' && i < N)
+	{
+		message[i] = c;
+		i++;
+	}
+
+	printf("In B1FF-speak: ");
+
+	for (int i = 0; i < N; i++)
+	{
+		switch(c = toupper(message[i]))
+		{
+			case 'A':
+				putchar('4');
+				break;
+			case 'B':
+				putchar('8');
+				break;
+			case 'E':
+				putchar('3');
+				break;
+			case 'I':
+				putchar('1');
+				break;
+			case 'O':
+				putchar('0');
+				break;
+			case 'S':
+				putchar('5');
+				break;
+			default:
+				putchar(c);
+				break;
+		}
+	}
+	printf("!!!!!!!!!!\n");
 
 	return 0;
 }
