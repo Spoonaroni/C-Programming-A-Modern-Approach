6c5b6ae7a991b4204b34d71f463efa9aa340581a
Spoonaroni <163045653+Spoonaroni@users.noreply.github.com>
2024-11-15 19:12:54 -0500

    Finish project 8.03

--- 08/projects/03/3.c
+++ 08/projects/03/3.c
@@ -1,7 +1,38 @@
+/* Checks numbers for repeated digits */
+
+#include <stdbool.h> /* C99 only */
 #include <stdio.h>
 
 int main(void)
 {
+	int digit_seen[10] = {0}, digit;
+	long n;
+
+	printf("Enter a number (0 to terminate): ");
+	scanf("%ld", &n);
+
+	while (n > 0)
+	{
+		while (n > 0)
+		{
+			digit = n % 10;
+			digit_seen[digit] += 1;
+			n /= 10;
+		}
+
+		printf("Digits:       0  1  2  3  4  5  6  7  8  9\n");
+		printf("Occurrences:  ");
+
+		for (int i = 0; i < 10; i++)
+		{
+			printf("%d  ", digit_seen[i]);
+			digit_seen[i] = 0;
+		}
+		printf("\n\n");
+
+		printf("Enter a number (0 to terminate): ");
+		scanf("%ld", &n);
+	}
 
 
 	return 0;
