ceee3d1c9955509fb8ec2f744292fea60be45bfb
Spoonaroni <163045653+Spoonaroni@users.noreply.github.com>
2024-11-16 19:26:13 -0500

    Finish project 8.07

--- 08/projects/07/7.c
+++ 08/projects/07/7.c
@@ -1,8 +1,46 @@
 #include <stdio.h>
 
+#define N 5
+
 int main(void)
 {
+	int sums[N][N] = {0}, row = 0, col = 0;
 
+	for (int i = 0; i < N; i++)
+	{
+		printf("Enter row %d: ", (i + 1));
+
+		for (int j = 0; j < N; j++)
+		{
+			scanf("%d", &sums[i][j]);
+		}
+	}
+
+	printf("\nRow totals: ");
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			row += sums[i][j];
+		}
+		printf("%d ", row);
+		row = 0;
+	}
+
+	printf("\nColumn totals: ");
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			col += sums[j][i];
+		}
+		printf("%d ", col);
+		col = 0;
+	}
+
+	printf("\n");
 
 	return 0;
 }
