487d41a56a1a17e881c109bd3c023427e16f867e
Spoonaroni <163045653+Spoonaroni@users.noreply.github.com>
2024-11-24 17:33:13 -0500

    Finish project 8.14

--- 08/projects/14/14.c
+++ 08/projects/14/14.c
@@ -2,7 +2,46 @@
 
 int main(void)
 {
+	char c, termChar, words[100] = {0};
+	int i = 0, j = 0;
 
+	printf("Enter a sentence: ");
+	c = getchar();
+	while (c != '.' && c != '?' && c != '!')
+	{
+		words[i] = c;
+		c = getchar();
+		i++;
+	}
+	termChar = c;
+	words[i] = '\0';
+
+	printf("Reversal of sentence: ");
+	for (i -= 1; i >= 0; i--)
+	{
+		if (words[i] == ' ' || i == 0)
+		{
+			j = i;
+			if (i == 0)
+			{
+				while (words[j] != ' ' && words[j] != '\0')
+				{
+					printf("%c", words[j]);
+					j++;
+				}
+			}
+			else
+			{
+				while (words[j + 1] != ' ' && words[j + 1] != '\0')
+				{
+					printf("%c", words[j + 1]);
+					j++;
+				}
+				printf(" ");
+			}
+		}
+	}
+	printf("%c\n", termChar);
 
 	return 0;
 }
