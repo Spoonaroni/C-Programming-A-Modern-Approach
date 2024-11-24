f27b737e8b04a2c0fb5ccfeb8ff5817265ed8fdc
Spoonaroni <163045653+Spoonaroni@users.noreply.github.com>
2024-11-23 19:06:43 -0500

    Finish project 8.13

--- 08/projects/13/13.c
+++ 08/projects/13/13.c
@@ -2,7 +2,19 @@
 
 int main(void)
 {
+	char c, d[20] = {0};
+	int i = 0;
 
+	printf("Enter a first and last name: ");
+	c = getchar(); // get initial
+	while ((d[i] = getchar()) != ' ') // skip first name and space
+		;
+	
+	printf("You entered the name: ");
+	while ((d[i] = getchar()) != '\n')
+		printf("%c", d[i++]);
+	
+	printf(", %c.\n", c); // print initial
 
 	return 0;
 }
