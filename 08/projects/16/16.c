1470da026ec4f8dd5212d5e71c2eb5821870dbc7
Spoonaroni <163045653+Spoonaroni@users.noreply.github.com>
2024-11-28 23:34:28 -0500

    Finish project 8.16

--- 08/projects/16/16.c
+++ 08/projects/16/16.c
@@ -1,8 +1,43 @@
+#include <ctype.h>
 #include <stdio.h>
 
 int main(void)
 {
+	int read[26] = {0};
+	char c;
 
+	printf("Enter first word: ");
+
+	while ((c = getchar()) != '\n')
+	{
+		if (isalpha(c))
+		{
+			c = toupper(c) - 65;
+			read[(int) c] += 1;
+		}
+	}
+
+	printf("Enter second word: ");
+
+	while ((c = getchar()) != '\n')
+	{
+		if (isalpha(c))
+		{
+			c = toupper(c) - 65;
+			read[(int) c] -= 1;
+		}
+	}
+
+	for (int i = 0; i < 26; i++)
+	{
+		if (read[i] != 0)
+		{
+			printf("The words are not anagrams.\n");
+			return 0;
+		}
+	}
+
+	printf("The words are anagrams.\n");
 
 	return 0;
 }
