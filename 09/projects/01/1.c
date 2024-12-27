a3ad3865896d8fe93b684c44e61c078bea208b5d
Spoonaroni <163045653+Spoonaroni@users.noreply.github.com>
2024-12-26 20:17:59 -0500

    Finish project 01

--- 09/projects/01/1.c
+++ 09/projects/01/1.c
@@ -1,8 +1,49 @@
+#include <ctype.h>
 #include <stdio.h>
+
+#define LEN 100
+
+void selection_sort(int a[], int n);
 
 int main(void)
 {
+    int n = 0, array[LEN];
 
+    printf("Enter a series of integers (Enter a non-integer to end): ");
+    for (int i = 0; i < LEN; i++) {
+        if (scanf(" %d", &array[i]) != 1)
+            break;
+        n++;
+    }
+
+    selection_sort(array, n);
+
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++)
+        printf(" %d", array[i]);
+    printf("\n");
 
 	return 0;
+}
+
+void selection_sort(int a[], int n)
+{
+    int lar = 0, temp, count;
+
+    if (n == 0)
+        return;
+
+    for (int i = 0; i < n; i++) {
+        if (a[i] > lar)
+        {
+            lar = a[i];
+            count = i;
+        }
+    }
+
+    temp = a[n - 1];
+    a[count] = temp;
+    a[n - 1] = lar;
+
+    selection_sort(a, n - 1);
 }
--- 09/projects/01/README.md
+++ 09/projects/01/README.md
@@ -1,5 +1,11 @@
 ## Project 9.01
 
+Write a program that asks the user to enter a series of integers (which it stores in an array), then sorts the integers by calling the funtion `selection_sort`. When given an array with `n` elements, `selection_sort` must do the following:  
 
+1. Search the array to find the largest element, then move it to the last position in the array.  
+
+2. Call itself recursively to sort the first `n - 1` elements of the array.  
 
 ## Solution
+
+See `1.c`
