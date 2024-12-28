83be6f41aaa1935854bab78eb140047f1325805b
Spoonaroni <163045653+Spoonaroni@users.noreply.github.com>
2024-12-28 17:35:47 -0500

    Finish project 02

--- 09/projects/02/2.c
+++ 09/projects/02/2.c
@@ -1,8 +1,35 @@
 #include <stdio.h>
 
+float taxCalc(float income);
+
 int main(void)
 {
+    float income;
 
+    printf("Enter amount of taxable income: ");
+    scanf("%f", &income);
 
-	return 0;
+    printf("Tax due: $%.2f\n", taxCalc(income));
+    
+    return 0;
+}
+
+float taxCalc(float income)
+{
+	float tax;
+
+	if (income < 750.00f)
+        tax = income * 0.01f;
+	else if (income < 2250.00f)
+        tax = 7.50f + ((income - 750.00f) * 0.02f);
+	else if (income < 3750.00f)
+        tax = 37.50f + ((income - 2250.00f) * 0.03f);
+	else if (income < 5250.00f)
+        tax = 82.50f + ((income - 3750.00f) * 0.04f);
+	else if (income  < 7000.00f)
+        tax = 142.50f + ((income - 5250.00f) * 0.05f);
+	else
+        tax = 230.00f + ((income - 7000.00f) * 0.06f);
+
+	return tax;
 }
