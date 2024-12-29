9bf73db09122fb57a6d8be560645073d275d05be
Spoonaroni <163045653+Spoonaroni@users.noreply.github.com>
2024-12-29 15:26:06 -0500

    Finish project 06

--- 09/projects/06/6.c
+++ 09/projects/06/6.c
@@ -1,8 +1,33 @@
 #include <stdio.h>
 
+int power(int a, int b);
+int compute_poly(int x);
+
 int main(void)
 {
+	int x;
 
+	printf("Enter the value of x: ");
+	scanf("%d", &x);
+
+	printf("Value of polynomial: %d\n", compute_poly(x));
 
 	return 0;
+}
+
+int power(int a, int b)
+{
+	if (b == 0)
+		return 1;
+	else
+		return a * power(a, b - 1);
+}
+
+int compute_poly(int x)
+{
+	int value;
+
+	value = (3 * power(x, 5)) + (2 * power(x, 4)) - (5 * power(x, 3)) - (power(x, 2)) + (7 * x) - 6;
+
+	return value;
 }
--- 09/projects/06/README.md
+++ 09/projects/06/README.md
@@ -1,5 +1,13 @@
 ## Project 9.06
 
+Write a function that computes the value of the following polynomial:
 
+```
+3x^5 + 2x^4 - 5x^3 - x^2 + 7x - 6
+```
+
+Write a program that asks the user to enter a value for `x`, calls the function to compute the value of the polynomial, and then displays the value returned by the function.
 
 ## Solution
+
+See `6.c`
