be8f86df11686262cef0b20878416555d5e4d6f7
Spoonaroni <163045653+Spoonaroni@users.noreply.github.com>
2024-12-29 18:03:08 -0500

    Finish project 08

--- 09/projects/08/8.c
+++ 09/projects/08/8.c
@@ -1,8 +1,75 @@
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int roll_dice(void);
+bool play_game(void);
 
 int main(void)
 {
+	int wins = 0, losses = 0;
+	char n;
+	srand((unsigned) time(NULL));
 
+	while (true) {
+		if (play_game()) 
+			wins++;
+		else 
+			losses++;
+		
+
+		printf("Play again? ");
+		scanf(" %c", &n);
+		if (n != 'y' && n != 'Y')
+			break;
+		printf("\n");
+	}
+
+	printf("Wins: %d  Losses: %d\n", wins, losses);
 
 	return 0;
+}
+
+int roll_dice(void)
+{
+	int num1 = (rand() % 6) + 1, 
+		num2 = (rand() % 6) + 1;
+
+	return num1 + num2;
+}
+
+bool play_game(void)
+{
+	int point, roll;
+
+	roll = roll_dice();
+	printf("You rolled: %d\n", roll);
+
+	if (roll == 7 || roll == 11) {
+		printf("You win!\n\n");
+		return true;
+	} else if (roll == 2 || roll == 3 || roll == 12) {
+		printf("You lose!\n\n");
+		return false;
+	} else {
+		point = roll;
+		printf("Your point is %d\n", point);
+	}
+
+	while (true) {
+		roll = roll_dice();
+		printf("You rolled: %d\n", roll);
+
+		if (roll == point) {
+			printf("You win!\n\n");
+			return true;
+		}
+		else if (roll == 7) {
+			printf("You lose!\n\n");
+			return false;
+		}
+	}
+
+	return false;
 }
