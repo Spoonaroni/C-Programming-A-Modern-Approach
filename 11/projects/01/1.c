2d6cc8eb5388a3d6861201926b316a5d049805be
Spoonaroni <163045653+Spoonaroni@users.noreply.github.com>
2025-02-01 19:57:23 -0500

    Finish project 11.01

--- 11/projects/01/1.c
+++ 11/projects/01/1.c
@@ -1,8 +1,34 @@
 #include <stdio.h>
 
+void pay_amount(int dollars, int *twenties, int *tens, int *fives, int *ones);
+
 int main(void)
 {
+    int value, twenties = 0, tens = 0, fives = 0, ones = 0;
 
+	printf("Enter a dollar amount: ");
+    scanf("%i", &value);
 
-	return 0;
+	pay_amount(value, &twenties, &tens, &fives, &ones);
+
+    printf("$20 bills: %d\n", twenties);
+    printf("$10 bills: %d\n", tens);
+    printf("$5 bills: %d\n", fives);
+    printf("$1 bills: %d\n", ones);
+
+    return 0;
+}
+
+void pay_amount(int dollars, int *twenties, int *tens, int *fives, int *ones)
+{
+    *twenties = dollars / 20;
+	dollars -= 20 * (dollars / 20);
+
+    *tens = dollars / 10;
+	dollars -= 10 * (dollars / 10);
+
+    *fives = 5 * (dollars/5);
+	dollars -= 5 * (dollars / 5);
+	
+	*ones = dollars;
 }
--- 11/projects/01/README.md
+++ 11/projects/01/README.md
@@ -1,5 +1,17 @@
 # Project 11.01
 
+Modify Programming Project 7 from Chapter 2 so that it includes the following function:
 
+```c
+void pay_amount(int dollar, int *twenties, int *tens,
+                int *fives, int *ones);
+```
+
+The function determines the smallest number of $20, $10, $5, and $1 bills necessary
+to pay the amount represented by the `dollars` parameter. The `twenties` parameter
+points to a variables in which the function will store the number of $20 bills required.
+The `tens`, `fives`, and `ones` parameters are similar.
 
 ## Solution
+
+See `1.c`
