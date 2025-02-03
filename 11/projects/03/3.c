dad0d493e7e58983fc334f069280e5261d6eaa55
Spoonaroni <163045653+Spoonaroni@users.noreply.github.com>
2025-02-03 02:27:40 -0500

    Finish project 11.03

--- 11/projects/03/3.c
+++ 11/projects/03/3.c
@@ -1,8 +1,34 @@
 #include <stdio.h>
 
+void reduce(int numerator, int denominator, int *reduced_numerator, int *reduced_denominator);
+
 int main(void)
 {
+    int num, den, n, m;
 
+    printf("Enter a fraction: ");
+    scanf("%d / %d", &num, &den);
 
-	return 0;
+    m = num;
+    n = den;
+
+	reduce(n, m, &num, &den);
+
+    printf("In lowest terms: %d/%d\n", num, den);
+    
+    return 0;
+}
+
+void reduce(int numerator, int denominator, int *reduced_numerator, int *reduced_denominator)
+{
+	int r;
+	while (numerator != 0)
+    {
+        r = denominator % numerator;
+        denominator = numerator;
+        numerator = r;
+    }
+
+    *reduced_numerator /= denominator;
+    *reduced_denominator /= denominator;
 }
