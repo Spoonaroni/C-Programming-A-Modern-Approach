c0b24ff6cab1d9118083825a4a9a5c9ee7b221c5
Spoonaroni <163045653+Spoonaroni@users.noreply.github.com>
2025-02-17 19:30:33 -0500

    Finsish project 12.03

--- 12/projects/03/3.c
+++ 12/projects/03/3.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 
+#define MAX 100
+
 int main(void)
 {
+	char c, charArray[MAX], *p = charArray;
 
+	printf("Enter a message: ");
+	while ((c = getchar()) != '\n' && p < charArray + MAX) {
+		*p++ = c;
+	}
+
+	p--;
+	printf("Reversal is: ");
+	while (p >= charArray) {
+		putchar(*p--);
+	}
+	printf("\n");
 
 	return 0;
 }
