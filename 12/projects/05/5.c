a12de9344a0412c202a6a66b094449d53b159e61
Spoonaroni <163045653+Spoonaroni@users.noreply.github.com>
2025-02-17 20:08:21 -0500

    Finsish project 12.05

--- 12/projects/05/5.c
+++ 12/projects/05/5.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
 
+#define MAX 100
+
 int main(void)
 {
+	char c, termChar, words[MAX] = {0}, *p, *q;
 
+	printf("Enter a sentence: ");
+	for (p = words; (c = getchar()) != '\n' && p < words + MAX; p++) {
+		if (c == '.' || c == '!' || c == '?') {
+			termChar = c;
+			break;
+		}
+		*p = c;
+	}
+
+	printf("Reversal of sentence: ");
+	while (p >= words) {
+		while (*--p != ' ' && p != words)
+			;
+		q = p == words ? words : p + 1;
+		while (*q != ' ' && *q != '\0')
+			putchar(*q++);
+		if (p >= words)
+			putchar(' ');
+	}
+	printf("%c\n", termChar);
 
 	return 0;
 }
--- 12/projects/05/README.md
+++ 12/projects/05/README.md
@@ -1,5 +1,8 @@
 # Project 12.05
 
-
+Modify Programming Project 14 from Chapter 8 so that it uses a pointer instead of
+an integer to keep track of the current position in the array that contains the sentence.
 
 ## Solution
+
+See `5.c`
