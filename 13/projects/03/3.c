22b68dc71f29f38e8b24391416a4f755fadd1bc9
Spoonaroni <163045653+Spoonaroni@users.noreply.github.com>
2025-03-12 22:35:51 -0400

    Finish project 13.03

--- 13/projects/03/3.c
+++ 13/projects/03/3.c
@@ -1,8 +1,34 @@
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define NUM_SUITS 4
+#define NUM_RANKS 13
 
 int main(void)
 {
+	bool in_hand[NUM_SUITS][NUM_RANKS] = {false};
+	int num_cards, rank, suit;
+	const char *rank_str[13] = {"Two", "Three", "Four", "Five", "Six", "Seven", "Eight",
+								"Nine", "Ten", "Jack", "Queen", "King", "Ace"};
+	const char *suit_str[4] = {"hearts",  "spades", "clubs", "diamonds"};
 
+	srand((unsigned) time(NULL));
+	
+	printf("Enter number of cards in hand: ");
+	scanf("%d", &num_cards);
+
+	printf("Your hand:\n");
+	while (num_cards > 0) {
+		suit = rand() % NUM_SUITS;		/* picks a random suit */
+		rank = rand() % NUM_RANKS;		/* picks a random rank */
+		if (!in_hand[suit][rank]) {
+			in_hand[suit][rank] = true;
+			num_cards--;
+			printf("%s of %s\n", rank_str[rank], suit_str[suit]);
+		}
+	}
 
 	return 0;
 }
--- 13/projects/03/README.md
+++ 13/projects/03/README.md
@@ -1,5 +1,20 @@
 # Project 13.03
 
+Modify the `deal.c` program of Section 8.2 so that is prints the full names of the
+cards it deals:
 
+```text
+Enter number of cards in hand: 5
+Your hand:
+Seven of clubs
+Two of spades
+Five of diamonds
+Ace of spades
+Two of hearts
+```
+
+*Hint*: Replace `rank_code` and `suit_code` by arrays containing pointers to strings.
 
 ## Solution
+
+See `3.c`
