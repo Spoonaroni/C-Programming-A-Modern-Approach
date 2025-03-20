05dec8c9249d75379d8564f05f4f769efdcea0f7
Spoonaroni <163045653+Spoonaroni@users.noreply.github.com>
2025-03-20 14:51:44 -0400

    Finish project 13.08

--- 13/projects/08/8.c
+++ 13/projects/08/8.c
@@ -1,8 +1,35 @@
+#include <ctype.h>
 #include <stdio.h>
+
+#define MAX 100
+
+int compute_scrabble_value(const char *word);
 
 int main(void)
 {
+	int sum;
+	char word[MAX];
 
+	printf("Enter a word: ");
+	scanf("%s", word);
+
+	sum = compute_scrabble_value(word);
+
+	printf("Scrabble value: %d\n", sum);
 
 	return 0;
+}
+
+int compute_scrabble_value(const char *word)
+{
+	int sum = 0,
+		values[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3,
+					1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+
+	while (*word) {
+		sum += values[toupper(*word) - 65];
+		word++;
+	}
+
+	return sum;
 }
--- 13/projects/08/README.md
+++ 13/projects/08/README.md
@@ -1,5 +1,13 @@
 # Project 13.08
 
+Modify Programming Project 5 from Chapter 7 so that is includes the following function:
 
+```c
+int compute_scrabble_value(const char *word);
+```
+
+The function returns the SCRABBLE value of the string pointed to by `word`.
 
 ## Solution
+
+See `8.c`
