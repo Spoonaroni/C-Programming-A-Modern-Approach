061e9aee21be58800b69995e166478d277822d0c
Spoonaroni <163045653+Spoonaroni@users.noreply.github.com>
2025-03-24 18:00:10 -0400

    Finish project 13.09

--- 13/projects/09/9.c
+++ 13/projects/09/9.c
@@ -1,8 +1,39 @@
+#include <ctype.h>
 #include <stdio.h>
+
+#define MAX 100
+
+int compute_vowel_count(const char *sentence);
 
 int main(void)
 {
+	int vCount = 0;
+	char c, s[MAX+1], *p = s;
 
+	printf("Enter a sentence: ");
+	while ((c = getchar()) != '\n' && p < s + MAX) {
+		*p++ = c;
+	}
+	*p = '\0';
+
+	vCount = compute_vowel_count(s);
+
+	printf("Your sentence contains %d vowels.\n", vCount);
 
 	return 0;
+}
+
+int compute_vowel_count(const char *sentence)
+{
+	char c;
+	int i = 0;
+
+	while (*sentence) {
+		c = toupper(*sentence);
+		if (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
+			i++;
+		sentence++;
+	}
+
+	return i;
 }
--- 13/projects/09/README.md
+++ 13/projects/09/README.md
@@ -1,5 +1,14 @@
 # Project 13.09
 
+Modify Programming Project 10 from Chapter 8 so that it includes the folliwng function:
 
+```c
+int compute_vowel_count(const char *sentence);
+```
+
+The function return the number of vowels in the string pointed to by the `sentence`
+parameter.
 
 ## Solution
+
+See `9.c`
