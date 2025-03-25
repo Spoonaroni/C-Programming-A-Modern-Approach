b29ab2ec2e04ef5820d1cc0d45148f738d156d47
Spoonaroni <163045653+Spoonaroni@users.noreply.github.com>
2025-03-25 17:36:16 -0400

    Finish project 13.11

--- 13/projects/11/11.c
+++ 13/projects/11/11.c
@@ -1,8 +1,42 @@
 #include <stdio.h>
 
+#define MAX 100
+
+double compute_average_word_length(const char *sentence);
+
 int main(void)
 {
+	float avg;
+	char sentence[MAX+1];
 
+	printf("Enter a sentence: ");
+	gets(sentence); 
+	//NOTE: gets() is deprecated, but to follow the book properly I will use it anyway.
+	//NOTE: The modern way of doing the same thing is with fgets(). "fgets(sentence, sizeof(sentence), stdin);"
+
+	avg = compute_average_word_length(sentence);
+
+	printf("Average word length: %.1f\n", avg);
 
 	return 0;
+}
+
+double compute_average_word_length(const char *sentence)
+{
+	double avg;
+	int len = 0, count = 1;
+	char c;
+
+	while (*sentence) {
+		c = *sentence;
+		if (c == ' ')
+			count++;
+		else
+			len++;
+		sentence++;
+	}
+
+	avg = (float) len / (float) count;
+
+	return avg;
 }
--- 13/projects/11/README.md
+++ 13/projects/11/README.md
@@ -1,5 +1,11 @@
 # Project 13.11
 
+Modify Programming Project 13 from Chapter 7 so that it includes the following function:
 
+```c
+double compute_average_word_length(const char *sentence);
+```
 
 ## Solution
+
+See `11.c`
