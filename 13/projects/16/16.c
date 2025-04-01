113899e137f9326a7cec6d7ad8c568b31c343c11
Spoonaroni <163045653+Spoonaroni@users.noreply.github.com>
2025-04-01 18:21:44 -0400

    Finish project 13.16

--- 13/projects/16/16.c
+++ 13/projects/16/16.c
@@ -1,8 +1,38 @@
 #include <stdio.h>
 
+#define MAX 100
+
+void reverse(char *message);
+
 int main(void)
 {
+	char c, charArray[MAX], *p = charArray;
 
+	printf("Enter a message: ");
+	while ((c = getchar()) != '\n' && p < charArray + MAX) {
+		*p++ = c;
+	}
+	*p = '\0';
+
+	reverse(charArray);
 
 	return 0;
+}
+
+void reverse(char *message)
+{
+	char temp, *first = message, *last = message;
+
+	while (*last)
+		last++;
+	last--;
+
+	while (first < last) {
+		temp = *first;
+		*first++ = *last;
+		*last-- = temp;
+	}
+	
+	printf("Reversal is: ");
+	printf("%s\n", message);
 }
