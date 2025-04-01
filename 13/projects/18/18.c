6ddadea639549ba59d3ee694f1f0e0fb14332ddf
Spoonaroni <163045653+Spoonaroni@users.noreply.github.com>
2025-04-01 19:50:50 -0400

    Finish project 13.18

--- 13/projects/18/18.c
+++ 13/projects/18/18.c
@@ -2,7 +2,15 @@
 
 int main(void)
 {
+	int month, day, year;
+	char listOfMonths[12][9] = {"January", "February", "March", "April",
+								"May", "June", "July", "August",
+								"September", "October", "November", "December"};
 
+	printf("Enter a date (mm/dd/yyyy): ");
+	scanf("%2d/%2d/%4d", &month, &day, &year);
+
+	printf("You entered the date %s %d, %d\n", listOfMonths[month-1], day, year);
 
 	return 0;
 }
