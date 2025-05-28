1e3bcaa842c7199a0c1142f5ead2cc18842561d8
Spoonaroni <163045653+Spoonaroni@users.noreply.github.com>
2025-05-28 10:07:42 -0400

    Finish project 16.05

--- 16/projects/05/5.c
+++ 16/projects/05/5.c
@@ -1,8 +1,48 @@
 #include <stdio.h>
 
+#define MAX 8
+
+struct flight {
+	int depart;
+	int arrive;
+} flights[MAX] = {
+	{480, 616},
+	{583, 712},
+	{679, 811},
+	{767, 900},
+	{840, 968},
+	{945, 1075},
+	{1140, 1280},
+	{1305, 1438}
+};
+
+void print_times(struct flight t);
+
 int main(void)
 {
+    int time, hour, min;
 
+    printf("Enter a 24-hour time: ");
+    scanf("%d : %d", &hour, &min);
+    time = hour * 60 + min;
 
-	return 0;
+    printf("Closest departure time is ");
+
+	for (int i = 0; i < MAX - 1; i++) {
+		if (time <= flights[i].depart + (flights[i + 1].depart - flights[i].depart)) {
+			print_times(flights[i]);
+			return 0;
+		}
+	}
+
+    return 0;
+}
+
+void print_times(struct flight t)
+{
+	printf("%d:%.2d %c.m., arriving at %d:%.2d %c.m.\n",
+			t.depart / 60 > 12 ? t.depart / 60 - 12 : t.depart / 60,
+			t.depart % 60, t.depart > 719 ? 'p' : 'a',
+			t.arrive / 60 > 12 ? t.arrive / 60 - 12 : t.arrive / 60,
+			t.arrive % 60, t.arrive > 719 ? 'p' : 'a');
 }
--- 16/projects/05/README.md
+++ 16/projects/05/README.md
@@ -1,5 +1,11 @@
 # Project 16.05
 
-
+Modify Programming Project 8 from Chapter 5 so that the times are stored in a
+single array. The elements of the array will be structures, each containing a
+departure time and the corresponding time. (Each time will be an integer,
+representing thenumber of minutes since midnight.) The program will use a loop
+to seach the array for the departue time closest to the time entered by the user.
 
 ## Solution
+
+See `5.c`
