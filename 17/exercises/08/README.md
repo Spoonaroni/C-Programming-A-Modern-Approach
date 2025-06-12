a8c4c769ddcd81f6979ba6e71712a89f25420733
Spoonaroni <163045653+Spoonaroni@users.noreply.github.com>
2025-06-12 17:08:20 -0400

    Finish exercise 17.08

--- 17/exercises/08/README.md
+++ 17/exercises/08/README.md
@@ -1,5 +1,13 @@
 # Exercise 17.08
 
-
+Section 15.02 describes a file, `stack.c`, that provides functions for storing
+integers in a stack. In that section, the stack was implemented as an array. Modify
+`stack.c` so that a stack is now stored as a linked list. Replace the `contents`
+and `top` variables by a single variable that pointers to the first node in the list
+(the "top" of the stack). Write the functions in `stack.c` so that they use this
+pointer. Remove the `is_full` function, instead having `push` return `true` (if
+memory was available to create a node) or `false` (if not).
 
 ## Solution
+
+See `stack.c`
---
+++ 17/exercises/08/stack.c
@@ -0,0 +1,62 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+// #include "stack.h"
+
+struct node {
+	int value;
+	struct node *next;
+};
+
+struct node *top = NULL;
+
+void make_empty(void)
+{
+	struct node *temp;
+
+	while (top != NULL) {
+		temp = top;
+		top = top->next;
+		free(temp);
+	}
+}
+
+bool is_empty(void)
+{
+	if (top == NULL)
+		return true;
+	else
+		return false;
+}
+
+bool push(int i)
+{
+	struct node *new_node = malloc(sizeof(struct node));
+
+	if (new_node == NULL)
+		return false;
+	
+	new_node->value = i;
+	new_node->next = top;
+	top = new_node;
+
+	return true;
+}
+
+int pop(void)
+{
+	struct node *temp;
+	int i;
+
+	if (is_empty()) {
+		printf("Error: Stack underflow.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	i = top->value;
+	temp = top;
+	top = top->next;
+	free(temp);
+
+	return i;
+}
