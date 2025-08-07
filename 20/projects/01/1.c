df013da9ccce8d85f6daa58940bc16fb86618569
Spoonaroni <163045653+Spoonaroni@users.noreply.github.com>
2025-08-07 14:54:01 -0400

    Finish project 20.01

--- 20/projects/01/1.c
+++ 20/projects/01/1.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 
+union float_union {
+	float f;
+	struct float_struct {
+		unsigned int frac: 23, expo: 8, sign: 1;
+	} float_s;
+} my_float;
+
 int main(void)
 {
+	my_float.float_s.sign = 1;
+	my_float.float_s.expo = 128;
+	my_float.float_s.frac = 0;
 
+	printf("%.1f\n", my_float.f);
 
 	return 0;
 }
