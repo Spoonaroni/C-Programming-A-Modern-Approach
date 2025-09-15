c4f8a9f3f9e56dab905de2b07791cae47eaf6ce5
Spoonaroni <163045653+Spoonaroni@users.noreply.github.com>
2025-09-15 14:12:27 -0400

    Finish project 22.01

--- 22/projects/01/1.c
+++ 22/projects/01/1.c
@@ -1,8 +1,41 @@
+/*********************************************************
+ * From C PROGRAMMING: A MODERN APPROACH, Second Edition *
+ * By K. N. King                                         *
+ * Copyright (c) 2008, 1996 W. W. Norton & Company, Inc. *
+ * All rights reserved.                                  *
+ * This program may be freely distributed for class use, *
+ * provided that this copyright notice is retained.      *
+ *********************************************************/
+
+/* canopen.c (Chapter 22, page 547) */
+/* Checks whether a file can be opened for reading */
+
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void)
+int main(int argc, char *argv[])
 {
+	FILE *fp;
+	bool fail = false;
 
+	if (argc < 2) {
+		printf("usage: canopen filename(s)\n");
+		exit(EXIT_FAILURE);
+	}
 
-	return 0;
+	for (int i = 1; i < argc; i++) {
+		if ((fp = fopen(argv[i], "r")) == NULL) {
+			printf("%s can't be opened\n", argv[i]);
+			fail = true;
+		} else {
+			printf("%s can be opened\n", argv[i]);
+			fclose(fp);
+		}
+	}
+
+	if (fail == true)
+		exit(EXIT_FAILURE);
+
+	exit(EXIT_SUCCESS);
 }
