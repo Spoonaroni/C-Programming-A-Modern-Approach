0fdb8c6923247799abb3ff11b78afed855f1d3cd
Spoonaroni <163045653+Spoonaroni@users.noreply.github.com>
2025-09-15 15:34:32 -0400

    Finish project 22.02

--- 22/projects/02/2.c
+++ 22/projects/02/2.c
@@ -1,8 +1,30 @@
+#include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void)
+int main(int argc, char *argv[])
 {
+	FILE *fp;
+	int ch;
 
+	if (argc != 2) {
+		fprintf(stderr, "Usage: upper filename\n");
+		exit(EXIT_FAILURE);
+	}
 
+	if ((fp = fopen(argv[1], "r+")) == NULL) {
+		fprintf(stderr, "%s can't be opened\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	while ((ch = getc(fp)) != EOF) {
+		if (isalpha(ch) && islower(ch)) {
+			putc(toupper(ch), stdout);
+		} else {
+			putc(ch, stdout);
+		}
+	}
+
+	fclose(fp);
 	return 0;
 }
--- 22/projects/02/README.md
+++ 22/projects/02/README.md
@@ -1,5 +1,9 @@
 # Project 22.02
 
-
+Write a program that converts all letters in a file to upper case. (Characters
+other than letters shouldn't be changed.) The program should obtain the file
+name from the command line and write its ouput to `stdout`.
 
 ## Solution
+
+See `2.c`
