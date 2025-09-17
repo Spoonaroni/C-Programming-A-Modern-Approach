3a5cf16ea5f593cc69570a2c72f57ef0b6875cf1
Spoonaroni <163045653+Spoonaroni@users.noreply.github.com>
2025-09-17 19:09:20 -0400

    Finish project 22.05

--- 22/projects/05/5.c
+++ 22/projects/05/5.c
@@ -1,8 +1,47 @@
+/*********************************************************
+ * From C PROGRAMMING: A MODERN APPROACH, Second Edition *
+ * By K. N. King                                         *
+ * Copyright (c) 2008, 1996 W. W. Norton & Company, Inc. *
+ * All rights reserved.                                  *
+ * This program may be freely distributed for class use, *
+ * provided that this copyright notice is retained.      *
+ *********************************************************/
+
+/* xor.c page 515 */
+/* Performs XOR encryption */
+
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void)
+#define KEY '&'
+
+int main(int argc, char *argv[])
 {
+	FILE *orig_file, *new_file;
+	int ch;
 
+	if (argc != 3) {
+		fprintf(stderr, "Usage: xor orig_file new_file\n");
+		exit(EXIT_FAILURE);
+	}
 
+	if ((orig_file = fopen(argv[1], "rb")) == NULL) {
+		fprintf(stderr, "Error: %s can't be opened\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	if ((new_file = fopen(argv[2], "wb")) == NULL) {
+		fprintf(stderr, "Error: %s can't be opened\n", argv[2]);
+		fclose(orig_file);
+		exit(EXIT_FAILURE);
+	}
+
+	while ((ch = getc(orig_file)) != EOF) {
+		ch ^= KEY;
+		putc(ch, new_file);
+	}
+
+	fclose(orig_file);
+	fclose(new_file);
 	return 0;
 }
