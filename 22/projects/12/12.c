c5f6aef4689c34a8b1b458fe6d1e3fbca502964d
Spoonaroni <163045653+Spoonaroni@users.noreply.github.com>
2025-10-01 13:55:23 -0400

    Finish project 22.12

--- 22/projects/12/12.c
+++ 22/projects/12/12.c
@@ -1,8 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void)
+#define LINE_SIZE 25
+
+int main(int argc, char *argv[])
 {
+	FILE *fp;
+    int itemNum, m, d, y;
+    float unitPrice;
+	char str[LINE_SIZE+1];
 
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s filename\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
 
-	return 0;
+	if ((fp = fopen(argv[1], "r")) == NULL) {
+		fprintf(stderr, "Error: %s can't be opened\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	printf("Item\t\t\tUnit\t\t\tPurchase\n");
+	printf("\t\t\tPrice\t\t\tDate\n");
+
+	fgets(str, sizeof(str), fp);
+	while (sscanf(str, "%d,%f,%d/%d/%d", &itemNum, &unitPrice, &m, &d, &y) == 5) {
+		printf("%-4d\t\t\t$%7.2f\t\t%d/%d/%d\n", itemNum, unitPrice, m, d, y);
+		fgets(str, sizeof(str), fp);
+	}
+
+    return 0;
 }
--- 22/projects/12/README.md
+++ 22/projects/12/README.md
@@ -1,5 +1,31 @@
 # Project 22.12
 
+Modify Programming Project 2 from Chapter 3 so that the program reads a series
+of items from a file and displays the data in columns. Each line of the file
+will have the following form:
 
+```txt
+item,price,mm/dd/yyyy
+```
+
+For example, suppose that the file contains the following lines:
+
+```txt
+583,13.5,10/24/2005
+3912,599.99,7/27/2008
+```
+
+The output of the program should have the following appearance:
+
+```txt
+Item            Unit            Purchase
+                Price           Date
+583             $  13.50        10/24/2005
+3912            $ 599.99        7/27/2008
+```
+
+Have the program obtain the file name from the command line.
 
 ## Solution
+
+See `12.c`
