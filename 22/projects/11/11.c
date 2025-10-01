8b617542aacc657512dc2ecb601f05faee24be92
Spoonaroni <163045653+Spoonaroni@users.noreply.github.com>
2025-10-01 12:57:44 -0400

    Finish project 22.11

--- 22/projects/11/11.c
+++ 22/projects/11/11.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void)
+
+int main(int argc, char *argv[])
 {
+	char *months[] = { "January", "February", "March", "April",
+						"May", "June", "July", "August",
+						"September", "October", "November", "December" };
+	int month, day, year;
 
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s date\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
 
-	return 0;
+	if (sscanf(argv[1], "%d/%d/%d", &month, &day, &year) == 3)
+		printf("%s %d, %4d\n", months[month-1], day, year);
+	else if (sscanf(argv[1], "%d-%d-%d", &month, &day, &year) == 3)
+		printf("%s %d, %d\n", months[month-1], day, year);
+	else {
+		fprintf(stderr, "Error: Date not in proper format\n");
+		exit(EXIT_FAILURE);
+	}
+
+	exit(EXIT_SUCCESS);
 }
